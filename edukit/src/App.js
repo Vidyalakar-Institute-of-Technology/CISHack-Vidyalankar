e52854700d9e7609ee19f304d22feccdd2a4c76e
prerana1821 <prerananawar1@gmail.com>
2021-04-25 08:56:40 +0530

    Added Project

---
+++ edukit/src/App.js
@@ -0,0 +1,52 @@
+import { Routes, Route } from "react-router-dom";
+import { VideoListing } from "./VideoListing";
+import { Header } from "./Header";
+import { LikedVideos } from "./LikedVideos";
+import { SavedVideos } from "./SavedVideos";
+import { PlayList } from "./Playlist";
+import { Video } from "./Videos";
+import { History } from "./History";
+// import { SideBar } from "./Sidebar";
+import "./App.css";
+import { Quiz } from "./Quizs/Quiz";
+import { Home } from "./Home/Home";
+import FlutterQuiz from "./Quizs/flutterQuiz";
+import AngularQuiz from "./Quizs/angularQuiz";
+import ReactQuiz from "./Quizs/reactQuiz";
+import LaravelQuiz from "./Quizs/laravelQuiz";
+import VueQuiz from "./Quizs/vueQuiz";
+
+function App() {
+  return (
+    <div className='App' id='top'>
+      <Header />
+      <div className='main'>
+        {/* <SideBar /> */}
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/courses' element={<VideoListing />}></Route>
+          <Route path='courses/video/:videoId' element={<Video />}></Route>
+          <Route path='/playlist' element={<PlayList />}></Route>
+          <Route path='/quizes' element={<Quiz />}></Route>
+          <Route path='/history' element={<History />}></Route>
+          <Route path='/liked-videos' element={<LikedVideos />}></Route>
+          <Route path='/saved-videos' element={<SavedVideos />}></Route>
+          <Route path='quizes/Flutter' element={<FlutterQuiz />}></Route>
+          <Route path='quizes/AngularJS' element={<AngularQuiz />}></Route>
+          <Route path='quizes/React' element={<ReactQuiz />}></Route>
+          <Route path='quizes/Laravel-6' element={<LaravelQuiz />}></Route>
+          <Route path='quizes/VueJS' element={<VueQuiz />}></Route>
+        </Routes>
+        <a href='#top'>
+          <div className='btn-top badge-av'>
+            <div className='avatar avatar-top av-pink'>
+              <i className='fas fa-lg  fa-arrow-up'></i>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
